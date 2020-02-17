46ecea000de357e5b025bb59684d978dc746d4f9
narongsak keawmanee <klogic@hotmail.co.th>
2020-02-17 13:22:01 +0700

    init 3 window

---
+++ main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  win.loadFile("index.html");
+}
+function createWindow2() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.loadFile("index2.html");
+}
+function createWindow3() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.loadFile("index3.html");
+}
+app.whenReady().then(createWindow);
+app.whenReady().then(createWindow2);
+app.whenReady().then(createWindow3);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
